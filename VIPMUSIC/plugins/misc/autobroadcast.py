987d12e9f2ed2445faf77899013dfc8ace4b97ef
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2024-02-06 13:03:48 +0530

    Update autobroadcast.py

--- VIPMUSIC/plugins/misc/autobroadcast.py
+++ VIPMUSIC/plugins/misc/autobroadcast.py
@@ -49,8 +49,8 @@ async def continuous_broadcast():
     while True:
         await send_message_to_chats()
 
-        # Wait for 500 seconds before next broadcast
-        await asyncio.sleep(500)
+        # Wait for 1000 seconds before next broadcast
+        await asyncio.sleep(1000)
 
 # Start the continuous broadcast loop if AUTO_BROADCAST is True
 if AUTO_GCASTS:  
